b9c89a9cc4fbdc84293528d4e2b14191a253df73
kompany-sde <77177020+kompany-sde@users.noreply.github.com>
2022-09-08 11:51:58 +0100

    Update Accounts.tsx

--- features/account/components/Accounts.tsx
+++ features/account/components/Accounts.tsx
@@ -3,25 +3,6 @@ import { useGetAccountsQuery } from '../api';
 import { useAccount } from '../hooks/useAccount';
 import { AccountList } from './AccountList';
 
-const data = [
-  {
-    accountUid: 'bd595ecb-5b67-4f4d-8b62-d9ac5505cf9c',
-    accountType: 'PRIMARY',
-    defaultCategory: 'ac94fd6a-cf00-4428-b34a-aa8d7383cf27',
-    currency: 'GBP',
-    createdAt: '2022-08-20T05:55:38.393Z',
-    name: 'Personal',
-  },
-  {
-    accountUid: '82fa3d90-7d59-4ec6-a61a-32a1da846ea8',
-    accountType: 'ADDITIONAL',
-    defaultCategory: '89fbb704-5f61-48f9-922f-aa066b01ea3c',
-    currency: 'EUR',
-    createdAt: '2022-08-20T06:03:18.227Z',
-    name: 'Euro',
-  },
-];
-
 export const Accounts = () => {
   const { data, isError, isLoading, isUninitialized } = useGetAccountsQuery();
   const { selectedAccount, setSelectedAccount } = useAccount();
